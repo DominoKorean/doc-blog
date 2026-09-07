71aab8738bfeede536c17c742198ed3d978acf10
Hongjinha00 <110039366+DominoKorean@users.noreply.github.com>
2026-09-07 23:47:46 +0900

    config를 type script로 번경

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
   sitemap: {
     hostname: 'https://dominokorean.github.io/doc-blog'
   },
-  title: "도미노한국인 문서 블로그",
-  description: "문서를 위한 블로그",
+  title: '도미노한국인 문서 블로그',
+  description: '문서를 위한 블로그',
   markdown: {
     config: (md) => {
       md.use(footnote)
